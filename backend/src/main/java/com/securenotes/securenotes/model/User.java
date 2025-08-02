f7c34ecc815f71fd7a6d8dbf9d3ab0ee3788b364
aakriti82 <aakritiyadav82@gmail.com>
2025-08-02 16:15:16 +0545

    initial changes

--- backend/src/main/java/com/securenotes/securenotes/model/User.java
+++ backend/src/main/java/com/securenotes/securenotes/model/User.java
@@ -1,31 +1,18 @@
 package com.securenotes.securenotes.model;
 
 import jakarta.persistence.*;
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
     private String username;
-
-    @JsonIgnore
-    @Column(nullable = false)
     private String password;
-
     private String role;
-
     private boolean locked = false;
     private int failedLoginAttempts = 0;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Note> notes;
 
     // --- Getters ---
     public Long getId() {
@@ -36,7 +23,6 @@ public class User {
         return username;
     }
 
-    @JsonIgnore
     public String getPassword() {
         return password;
     }
@@ -51,10 +37,6 @@ public class User {
 
     public int getFailedLoginAttempts() {
         return failedLoginAttempts;
-    }
-
-    public List<Note> getNotes() {
-        return notes;
     }
 
     // --- Setters ---
@@ -81,6 +63,4 @@ public class User {
     public void setFailedLoginAttempts(int failedLoginAttempts) {
         this.failedLoginAttempts = failedLoginAttempts;
     }
-
-    public void setNotes(List<Note> notes) {
-        this.notes = notes;
+}
