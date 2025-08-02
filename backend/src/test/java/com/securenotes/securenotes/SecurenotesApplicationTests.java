d7c57af3b744571b3edf450d1b6f53ab72ff8dee
aakriti82 <aakritiyadav82@gmail.com>
2025-08-02 16:58:47 +0545

    initial changes

--- backend/src/test/java/com/securenotes/securenotes/SecurenotesApplicationTests.java
+++ backend/src/test/java/com/securenotes/securenotes/SecurenotesApplicationTests.java
@@ -1,32 +1,13 @@
 package com.securenotes.securenotes;
 
-import com.securenotes.securenotes.model.User;
-import com.securenotes.securenotes.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SecurenotesApplicationTests {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void contextLoads() {
-        // Basic context load test
-    }
-
-    @Test
-    void adminUserShouldExist() {
-        assertThat(userRepository.findByUsername("admin")).isPresent();
-    }
-
-    @Test
-    void demoUserShouldExist() {
-        assertThat(userRepository.findByUsername("demo")).isPresent();
-    }
-
-
+}
